a637e9496f79a04d088cb47a012eebcabcd643db
dlhardi <hardisonderrick@gmail.com>
2020-12-14 20:55:52 -0500

    created empty div for answer content. created array of objects holding questions, answers, and the correct answer. created startTimer and renderNeztQuestion fucntion

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-console.log("hello world");
+var interval;
+var seconds = 60;
+var currentQuestionIndex = 0;
+
+// DOM elements
+var questionContentEl = document.getElementById("questionContent");
+var answerContentEl = document.getElementById("answerContent");
+
+var codeQuestions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts",
+  },
+
+  {
+    question: "The condition in an if /else statement is enclosed within ____",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correctAnswer: "parentheses",
+  },
+];
+
+var buttonEl = document.getElementById("submitBtn");
+buttonEl.setAttribute("style", "display: block; margin: auto;");
+
+buttonEl.addEventListener("click", function () {
+  alert("I've been clicked");
+  startTimer();
+  renderNextQuestion();
+});
+function renderNextQuestion() {
+  questionContentEl.textContent = codeQuestions[currentQuestionIndex].question;
+  answerContentEl.innerHTML = "";
+
+  for (i = 0; i < codeQuestions[currentQuestionIndex].choices.length; i++){
+
+    var selectButton = document.createElement("button");
+    selectButton.textContent=codeQuestions[currentQuestionIndex].choices[i];
+    answerContentEl.appendChild(selectButton);
+
+    //   var pTag = document.createElement("p");
+    //   pTag.textContent= codeQuestions[currentQuestionIndex].choices[i];
+    //   answerContentEl.appendChild(pTag);
+  }
+}
+function gameOver() {
+  alert("times up!");
+}
+function startTimer() {
+  interval = setInterval(function () {
+    seconds--;
+    if (seconds === 0) {
+      clearInterval(interval);
+      gameOver();
+    }
+    console.log(seconds);
+  }, 1000);
+}
